df6ece34825f560b6efc5fb729a91049d4d405cc
Parth Sonavane <sonavaneparthgit@gmail.com>
2026-07-10 20:25:47 +0530

    Upload Loops(Greetings).py from script

---
+++ Loops(Greetings).py
@@ -0,0 +1,9 @@
+l = ["Harry","Soham","Sachin","Rahul"]
+for li in l : 
+    if li.startswith("S"):
+        print("Welcome ! " ,li)
+for li in l : 
+    if not li.startswith("S"):
+        print("Welcome ! " ,li)
+    
+    
