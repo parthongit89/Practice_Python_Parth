992588d37e96e4f14bb0e095bcebcf1e25e684b6
Parth Sonavane <sonavaneparthgit@gmail.com>
2026-07-10 20:26:22 +0530

    Upload Unit conversions(Inhes to Cms).py from script

---
+++ Unit conversions(Inhes to Cms).py
@@ -0,0 +1,12 @@
+#  Inches to centimeter conv. viseversa
+def in_to_cm(inches):
+    centimeters = inches * 2.54
+    print("Centimeters =", centimeters)
+inches = float(input("Enter inches: "))
+in_to_cm(inches)
+
+def cm_to_in(inches):
+    centimeters = inches * 2.54
+    print("Centimeters =", centimeters)
+inches = float(input("Enter inches: "))
+cm_to_in(inches)
